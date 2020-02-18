824516b6306c5e78a72afa41613fe94961d21823
Baraou <corentin.baraou@epitech.eu>
2020-02-18 22:26:49 +0100

    Fini de jouer

--- src/print.ts
+++ src/print.ts
@@ -1,5 +1,5 @@
 import fs from 'fs';
 
 export function print(total: { pizzas: number[], slices: number }, filename: string | undefined) {
-  fs.writeFileSync(`./outputs/${filename}`, `${total.pizzas.length}\n${total.pizzas.join(' ')}`);
+  fs.writeFileSync(`./outputs/${filename}`, `${total.pizzas.length}\n${total.pizzas.join(' ')}\n`);
 }
--- src/resolve.ts
+++ src/resolve.ts
@@ -1,9 +1,22 @@
+function add(a: number, b: { id: number, value: number}) {
+  return a + b.value;
+}
+
 export function resolve(data: { maxSlices: number, typeCount: number, slicesCount: number[] } | null) {
   if (!data) return { pizzas: [], slices: 0 };
-  const total = data.slicesCount.reduce((total: { pizzas: number[], slices: number }, slices: number) => {
+  const total = data.slicesCount.reduceRight((total: { pizzas: number[], slices: number }, slices: number, key: number) => {
     const tmp = total.slices + slices;
-    if (tmp < data.maxSlices) return { pizzas: [...total.pizzas, slices], slices: total.slices + slices };
+    if (tmp < data.maxSlices) return { pizzas: [...total.pizzas, key], slices: total.slices + slices };
     return total;
   }, { pizzas: [], slices: 0 });
+  // let slices: { id: number, value: number}[] = [];
+  // for (let i = data.slicesCount.length - 1; i > -1; --i) {
+  //   const curr = data.slicesCount[i];
+  //   const total = slices.reduce(add, 0);
+  //   if (total === data.maxSlices) break;
+  //   if (total > data.maxSlices) {
+
+  //   }
+  // }
   return total;
 }
