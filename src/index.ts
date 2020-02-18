c493d4b3aaa5b568d0260b3ad7fb8ca19438e36b
Baraou <corentin.baraou@epitech.eu>
2020-02-18 20:54:48 +0100

    first push

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// import { parser } from "./parser";
+// import { print } from "./print";
+import { resolve } from "./resolve";
+
+console.log('start');
+
+resolve();
+// console.log(process.argv[3]);
+
+console.log('end');
