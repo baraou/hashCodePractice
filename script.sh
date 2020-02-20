b911a880795b0ca854156b84bde2d57f6a5b8e0f
Baraou <corentin.baraou@epitech.eu>
2020-02-20 19:45:21 +0100

    script lancement

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+npm run build
+npm run start ./inputs/a_example.txt
+npm run start ./inputs/b_read_on.txt
+npm run start ./inputs/c_incunabula.txt
+npm run start ./inputs/d_tough_choices.txt
+npm run start ./inputs/e_so_many_books.txt
+npm run start ./inputs/f_libraries_of_the_world.txt
