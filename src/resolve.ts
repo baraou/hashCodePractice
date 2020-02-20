6043f46bcabf25918e383b8db7c0d2a56d85fa33
Lakhdar <lsl@blockchain-studio.com>
2020-02-20 20:11:45 +0100

    remove library

--- src/resolve.ts
+++ src/resolve.ts
@@ -17,7 +17,23 @@ import { Output } from './print';
 //   days: number,
 // };
 
+function removeLibrary(toRemove: Library, libraries: Library[]): Library[] {
+  const bookIds = toRemove.books.map(book => book.id);
+  const filteredLibraries = libraries.map(library => ({
+    ...library,
+    books: library.books.filter(book => bookIds.indexOf(book.id) >= 0)
+  }));
+  return filteredLibraries;
+}
+
 export function resolve(input: Input) {
   const output: Output = { libraries: input.libraries };
+
+  const libraries = input.libraries;
+
+  while (!!libraries.length) {
+
+  }
+
   return output;
 }
