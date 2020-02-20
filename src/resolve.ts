9c036d59f799881baa099fa78728658e9741c4c3
Baraou <corentin.baraou@epitech.eu>
2020-02-20 20:32:25 +0100

    qsd

--- src/resolve.ts
+++ src/resolve.ts
@@ -1,4 +1,4 @@
-import { map, sumBy, orderBy } from 'lodash';
+import { map, sumBy, orderBy, find, filter } from 'lodash';
 
 import { Book, Library, Input } from './parser';
 import { Output } from './print';
@@ -29,21 +29,26 @@ function removeLibrary(toRemove: Library, libraries: Library[]): Library[] {
   return filteredLibraries;
 }
 
-function sort(libraries: Library[], days: number) {
-  return orderBy(map(libraries, (library, id) => {
-    const books = library.books.slice(0, days - library.signupTime);
+function sort(libraries: Library[], days: number): any {
+  const sorted = orderBy(map(libraries, (library, id) => {
+    const books = library.books.slice(0, (days - library.signupTime) * library.booksPerDay);
     const score = sumBy(books, 'score');
     return { libraryId: library.id, score };
   }), 'score');
+  return find(libraries, { id: sorted[0].libraryId });
 }
 
 export function resolve(input: Input) {
   const output: Output = { libraries: input.libraries };
 
-  const libraries = input.libraries;
+  let libraries = input.libraries;
+  let days = input.days;
 
   while (!!libraries.length) {
-
+    const best = sort(libraries, days);
+    days -= best.signupTime;
+    libraries = filter(libraries, (library) => library.id != best.id);
+    libraries = removeLibrary(best, libraries);
   }
 
   return output;
